f2235c02916e6365ff61c58a164e8486f52dfab4
kerim1 <kerim.meijer@di.huc.knaw.nl>
2025-05-27 14:37:53 +0200

    Add eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -7,15 +7,20 @@ import markdownIt from "markdown-it";
 // Import the markdown-it-attrs plugin to enable custom attributes (like classes) in Markdown
 import markdownItAttrs from "markdown-it-attrs";
 
+// Import the markdown-it-github-alerts plugin to add GitHub-style alerts in Markdown
+import MarkdownItGitHubAlerts from "markdown-it-github-alerts";
+
 // Export the Eleventy configuration function
 export default function (eleventyConfig) {
     // Set the Markdown library to markdown-it with the attrs plugin enabled
     // This allows you to add attributes to Markdown elements, e.g., {.class-name}
-    eleventyConfig.setLibrary("md", markdownIt({ html: true }).use(markdownItAttrs));
+    eleventyConfig.setLibrary("md", markdownIt({ html: true })
+        .use(markdownItAttrs)
+        .use(MarkdownItGitHubAlerts, { markers: '*' }));
 
     // Copy static assets directly to the output folder
     // This copies all files from src/static to the root of the output (_site) directory
-    eleventyConfig.addPassthroughCopy({ "src/static": "." });
+    eleventyConfig.addPassthroughCopy({ "src/static": "static" });
 
     // Copy the main CSS file to the root of the output folder
     eleventyConfig.addPassthroughCopy({ "src/main.css": "." });
